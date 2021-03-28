691cf875267a031343816201c362231657d6ae54
Defelo <elodef42@gmail.com>
2021-03-28 01:43:47 +0100

    Added redis to spam detection

--- moderation/spam_detection/cog.py
+++ moderation/spam_detection/cog.py
@@ -1,9 +1,12 @@
+import time
+
 from discord import Embed, Member, VoiceState
-from discord.ext import tasks, commands
+from discord.ext import commands
 from discord.ext.commands import Context, guild_only, UserInputError
 
 from PyDrocsid.cog import Cog
 from PyDrocsid.config import Contributor
+from PyDrocsid.redis import redis
 from PyDrocsid.translations import t
 from PyDrocsid.util import reply
 from cogs.library.moderation.spam_detection.colors import Colors
@@ -19,15 +22,6 @@ class SpamDetectionCog(Cog, name="Spam Detection"):
     CONTRIBUTORS = [Contributor.ce_phox, Contributor.Defelo]
     PERMISSIONS = SpamDetectionPermission
 
-    def __init__(self):
-        self.user_hops: dict[int, int] = {}
-
-    async def on_ready(self):
-        try:
-            self.hop_loop.start()
-        except RuntimeError:
-            self.hop_loop.restart()
-
     async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
         """
         Checks for channel-hopping
@@ -36,15 +30,25 @@ class SpamDetectionCog(Cog, name="Spam Detection"):
         if before.channel == after.channel:
             return
 
-        hops: int = self.user_hops.setdefault(member.id, 0) + 1
         max_hops: int = await SpamDetectionSettings.max_hops.get()
-
-        if max_hops <= 0 or self.user_hops[member.id] < max_hops:
-            self.user_hops[member.id] = hops
+        if max_hops <= 0:
             return
 
-        del self.user_hops[member.id]
-        embed = Embed(title=t.channel_hopping, color=Colors.SpamDetection)
+        ts = int(time.time() * 1000)
+        await redis.zremrangebyscore(key := f"channel_hops:user={member.id}", max=ts - 60 * 1000)
+        await redis.zadd(key, ts, ts)
+        await redis.expire(key, 60)
+        hops: int = await redis.zcount(key)
+
+        if hops <= max_hops:
+            return
+
+        if await redis.exists(key := f"channel_hops_alert_sent:user={member.id}"):
+            return
+
+        await redis.setex(key, 10, 1)
+
+        embed = Embed(title=t.channel_hopping, color=Colors.SpamDetection, description=t.hops_in_last_minute(cnt=hops))
         embed.add_field(name=tg.member, value=member.mention)
         embed.add_field(name=t.member_id, value=member.id)
         embed.set_author(name=str(member), icon_url=member.avatar_url)
@@ -52,14 +56,6 @@ class SpamDetectionCog(Cog, name="Spam Detection"):
             embed.add_field(name=t.current_channel, value=after.channel.name)
 
         await send_alert(member.guild, embed)
-
-    @tasks.loop(minutes=1)
-    async def hop_loop(self):
-        """
-        Once a minute, all possible channel hops are being reset
-        """
-
-        self.user_hops.clear()
 
     @commands.group(aliases=["spam", "sd"])
     @SpamDetectionPermission.read.check
