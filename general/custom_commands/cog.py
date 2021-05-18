fe7a05a940efe4443fc91bb78d947ee53a55bfa1
Defelo <elodef42@gmail.com>
2021-05-18 19:49:55 +0200

    Added basic custom commands

---
+++ general/custom_commands/cog.py
@@ -0,0 +1,70 @@
+from io import BytesIO
+from pathlib import Path
+
+import yaml
+from discord import Embed, File, TextChannel, Permissions
+from discord.ext import commands
+from discord.ext.commands import Context, CommandError
+
+from PyDrocsid.cog import Cog
+from PyDrocsid.command_edit import link_response
+from PyDrocsid.config import Contributor
+from PyDrocsid.emojis import name_to_emoji
+from PyDrocsid.translations import t
+from PyDrocsid.util import reply, docs
+
+tg = t.g
+t = t.custom_commands
+
+
+def create_custom_command(name: str, data: dict):
+    content = data.get("content", "")
+    file_name = None
+    file_content = None
+    embed = None
+
+    if file_data := data.get("file"):
+        path = Path(file_data)
+        file_name = path.name
+        file_content = path.read_bytes()
+
+    if embed_data := data.get("embed"):
+        embed = Embed.from_dict(embed_data)
+
+    async def send_message(ctx: Context, channel: TextChannel):
+        permissions: Permissions = channel.permissions_for(channel.guild.me)
+        if not permissions.send_messages:
+            raise CommandError(t.could_not_send_message(channel.mention))
+        if not permissions.embed_links:
+            raise CommandError(t.could_not_send_embed(channel.mention))
+
+        file = File(BytesIO(file_content), filename=file_name) if file_content else None
+        if ctx.channel.id == channel.id:
+            await reply(ctx, content, file=file, embed=embed)
+        else:
+            await link_response(ctx, await channel.send(content, file=file, embed=embed))
+            await ctx.message.add_reaction(name_to_emoji["white_check_mark"])
+
+    async def with_channel_parameter(_, ctx: Context, channel: TextChannel):
+        await send_message(ctx, channel)
+
+    async def without_channel_parameter(_, ctx: Context):
+        channel = ctx.bot.get_channel(channel_id) if (channel_id := data.get("channel")) else ctx.channel
+        await send_message(ctx, channel)
+
+    command = with_channel_parameter if data.get("channel_parameter", False) else without_channel_parameter
+    if description := data.get("description", ""):
+        command = docs(description)(command)
+    command = commands.command(name=name, aliases=data.get("aliases", []))(command)
+
+    return command
+
+
+class CustomCommandsCog(Cog, name="Custom Commands"):
+    CONTRIBUTORS = [Contributor.Defelo]
+
+    def __init__(self, commands_file: str):
+        with open(commands_file) as file:
+            cmds = yaml.safe_load(file)
+
+        self.__cog_commands__ = tuple(create_custom_command(name, data) for name, data in cmds.items())
