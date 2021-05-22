cf5d85cef00968a1979990e9765d63868be8e483
Florian Sabonchi <sabonchi@posteo.de>
2021-05-22 02:21:16 +0200

    replaced ctx.send with send_long_embed

--- moderation/user_notes/cog.py
+++ moderation/user_notes/cog.py
@@ -1,11 +1,13 @@
 from discord import Member, Embed
 from discord.ext import commands
-from discord.ext.commands import Context, UserInputError, guild_only, BadArgument
+from discord.ext.commands import Context, UserInputError, guild_only
 
 from PyDrocsid.cog import Cog
+from PyDrocsid.command import UserCommandError
 from PyDrocsid.config import Contributor
 from PyDrocsid.converter import UserMemberConverter
 from PyDrocsid.database import db, filter_by
+from PyDrocsid.embeds import send_long_embed
 from PyDrocsid.emojis import name_to_emoji
 from PyDrocsid.translations import t
 from cogs.library.moderation.user_notes.models import UserNote
@@ -42,7 +44,7 @@ class UserNoteCog(Cog, name="User notes"):
     async def remove_user_note(self, ctx: Context, message_id: str):
         user_notes = await db.get(UserNote, message_id=message_id)
         if not user_notes:
-            raise BadArgument(t.message_not_found)
+            raise UserCommandError(t.message_not_found)
         await db.delete(user_notes)
         await ctx.message.add_reaction(name_to_emoji["white_check_mark"])
 
@@ -55,4 +57,4 @@ class UserNoteCog(Cog, name="User notes"):
             embed.add_field(name=t.message, value=note.message, inline=True)
             embed.add_field(name=t.timestamp, value=note.timestamp.strftime("%d.%m.%Y %H:%M:%S"), inline=True)
             embed.add_field(name=t.author, value=note.author, inline=True)
-        await ctx.send(embed=embed)
+        await send_long_embed(ctx, embed)
