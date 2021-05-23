84d12cb77779df05af95b7c5bea63bb3432ed1ca
Houseplant1 <77019113+Houseplant1@users.noreply.github.com>
2021-05-23 20:25:08 +0200

    Update cog.py

--- integrations/wikipedia/cog.py
+++ integrations/wikipedia/cog.py
@@ -66,7 +66,8 @@ class WikipediaCog(Cog, name="Wikipedia"):
             await ctx.send(
                 embed=make_embed(
                     title=title,
-                    content="Wikipedia is not available currently!" " Try again later.",
+                    content="Wikipedia is not available currently!" 
+                            " Try again later.",
                     color=Colors.Wiki,
                     requested_by=ctx.author,
                 ),
