dd4836a77f356befb19a4ffc0cafd614112e39b2
Defelo <elodef42@gmail.com>
2021-06-02 17:18:52 +0200

    Added checks and warnings to custom commands

--- general/custom_commands/cog.py
+++ general/custom_commands/cog.py
@@ -4,9 +4,10 @@ from pathlib import Path
 from typing import Optional
 
 import yaml
-from discord import Embed, File, TextChannel, Permissions, NotFound, Forbidden
+from PyDrocsid.logger import get_logger
+from discord import Embed, File, TextChannel, NotFound, Forbidden
 from discord.ext import commands
-from discord.ext.commands import Context, CommandError
+from discord.ext.commands import Context
 
 from PyDrocsid.cog import Cog
 from PyDrocsid.command import reply, docs, confirm
@@ -15,6 +16,9 @@ from PyDrocsid.config import Contributor
 from PyDrocsid.emojis import name_to_emoji
 from PyDrocsid.permission import BasePermission
 from PyDrocsid.translations import t
+from PyDrocsid.util import check_message_send_permissions
+
+logger = get_logger(__name__)
 
 tg = t.g
 t = t.custom_commands
@@ -35,11 +39,7 @@ def create_custom_command(name: str, data: dict, permission: Optional[BasePermis
         embed = Embed.from_dict(embed_data)
 
     async def send_message(ctx: Context, channel: TextChannel):
-        permissions: Permissions = channel.permissions_for(channel.guild.me)
-        if not permissions.send_messages:
-            raise CommandError(t.could_not_send_message(channel.mention))
-        if not permissions.embed_links:
-            raise CommandError(t.could_not_send_embed(channel.mention))
+        check_message_send_permissions(channel, check_file=bool(file_content), check_embed=bool(embed))
 
         if bool(data.get("requires_confirmation", permission is not None)):
             conf_embed = Embed(title=t.confirmation, description=t.confirm(name, channel.mention))
@@ -100,6 +100,7 @@ class CustomCommandsCog(Cog, name="Custom Commands"):
         while path_list:
             path = path_list.pop()
             if not path.exists():
+                logger.warning(f"{path} does not exist")
                 continue
 
             if path.is_dir():
