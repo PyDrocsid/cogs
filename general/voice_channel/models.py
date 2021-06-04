f93138f9b31167a7c198961bee3c444cd4ad8cdd
Defelo <elodef42@gmail.com>
2021-06-04 21:10:25 +0200

    Added new basic voice channels (wip)

--- general/voice_channel/models.py
+++ general/voice_channel/models.py
@@ -1,56 +1,70 @@
 from __future__ import annotations
 
-from typing import Union
+from datetime import datetime
+from typing import Union, Optional
+from uuid import uuid4
 
-from PyDrocsid.database import db, select
-from sqlalchemy import Column, Integer, String, BigInteger, Boolean
+from sqlalchemy import Column, BigInteger, Boolean, DateTime, ForeignKey, String
+from sqlalchemy.orm import relationship
+
+from PyDrocsid.database import db
 
 
-class DynamicVoiceChannel(db.Base):
-    __tablename__ = "dynamic_voice_channel"
+class DynGroup(db.Base):
+    __tablename__ = "dynvoice_group"
+
+    id: Union[Column, int] = Column(String(36), primary_key=True, unique=True)
+    channels: list[DynChannel] = relationship("DynChannel", back_populates="group", cascade="all, delete")
+
+    @staticmethod
+    async def create(channel_id: int) -> DynGroup:
+        group = DynGroup(id=str(uuid4()))
+        group.channels.append(await DynChannel.create(channel_id, group.id))
+        await db.add(group)
+        return group
+
+
+class DynChannel(db.Base):
+    __tablename__ = "dynvoice_channel"
 
     channel_id: Union[Column, int] = Column(BigInteger, primary_key=True, unique=True)
-    group_id: Union[Column, int] = Column(Integer)
-    text_chat_id: Union[Column, int] = Column(BigInteger)
-    owner: Union[Column, int] = Column(BigInteger)
+    text_id: Union[Column, int] = Column(BigInteger)
+    locked: Union[Column, bool] = Column(Boolean)
+    group_id: Union[Column, int] = Column(String(36), ForeignKey("dynvoice_group.id"))
+    group: DynGroup = relationship("DynGroup", back_populates="channels")
+    owner_id: Union[Column, int] = Column(String(36), ForeignKey("dynvoice_channel_member.id"))
+    owner: Optional[DynChannelMember] = relationship("DynChannelMember", foreign_keys=[owner_id])
+    members: list[DynChannelMember] = relationship(
+        "DynChannelMember",
+        foreign_keys=lambda: [DynChannelMember.channel_id],
+        back_populates="channel",
+        cascade="all, delete",
+        order_by="DynChannelMember.timestamp",
+    )
 
     @staticmethod
-    async def create(channel_id: int, group_id: int, text_chat_id: int, owner: int) -> DynamicVoiceChannel:
-        row = DynamicVoiceChannel(channel_id=channel_id, group_id=group_id, text_chat_id=text_chat_id, owner=owner)
-        await db.add(row)
-        return row
+    async def create(channel_id: int, group_id: int) -> DynChannel:
+        channel = DynChannel(channel_id=channel_id, text_id=None, locked=False, group_id=group_id)
+        await db.add(channel)
+        return channel
+
+
+class DynChannelMember(db.Base):
+    __tablename__ = "dynvoice_channel_member"
+
+    id: Union[Column, int] = Column(String(36), primary_key=True, unique=True)
+    member_id: Union[Column, int] = Column(BigInteger)
+    channel_id: Union[Column, int] = Column(BigInteger, ForeignKey("dynvoice_channel.channel_id"))
+    channel: DynChannel = relationship("DynChannel", foreign_keys=[channel_id], back_populates="members")
+    timestamp: Union[Column, datetime] = Column(DateTime)
 
     @staticmethod
-    async def change_owner(channel_id: int, owner: int):
-        row: DynamicVoiceChannel = await db.first(select(DynamicVoiceChannel).filter_by(channel_id=channel_id))
-        row.owner = owner
-
-
-class DynamicVoiceGroup(db.Base):
-    __tablename__ = "dynamic_voice_group"
-
-    id: Union[Column, int] = Column(Integer, primary_key=True, unique=True)
-    name: Union[Column, str] = Column(String(32))
-    channel_id: Union[Column, int] = Column(BigInteger)
-    public: Union[Column, bool] = Column(Boolean)
-
-    @staticmethod
-    async def create(name: str, channel_id: int, public: bool) -> DynamicVoiceGroup:
-        row = DynamicVoiceGroup(name=name, channel_id=channel_id, public=public)
-        await db.add(row)
-        return row
-
-
-class RoleVoiceLink(db.Base):
-    __tablename__ = "role_voice_link"
-
-    role: Union[Column, int] = Column(BigInteger, primary_key=True)
-    voice_channel: Union[Column, int] = Column(BigInteger, primary_key=True)
-
-    @staticmethod
-    async def create(role: int, voice_channel: int) -> RoleVoiceLink:
-        link = RoleVoiceLink(role=role, voice_channel=voice_channel)
-
-        await db.add(link)
-
-        return link
+    async def create(member_id: int, channel_id: int) -> DynChannelMember:
+        member = DynChannelMember(
+            id=str(uuid4()),
+            member_id=member_id,
+            channel_id=channel_id,
+            timestamp=datetime.utcnow(),
+        )
+        await db.add(member)
+        return member
