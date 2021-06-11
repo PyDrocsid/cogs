d05534cbff0062e14faedfaa60f9a5dd8edd4c65
Defelo <elodef42@gmail.com>
2021-06-11 12:40:51 +0200

    Fixed and improved user notes

--- moderation/user_notes/models.py
+++ moderation/user_notes/models.py
@@ -1,8 +1,9 @@
-import secrets
+from __future__ import annotations
+
 from datetime import datetime
 from typing import Union
 
-from sqlalchemy import Column, Integer, Text, BigInteger, DateTime, String, BINARY
+from sqlalchemy import Column, Text, BigInteger, DateTime
 
 from PyDrocsid.database import db
 
@@ -10,18 +11,18 @@ from PyDrocsid.database import db
 class UserNote(db.Base):
     __tablename__ = "user_notes"
 
-    message_id: Union[Column, int] = Column(BigInteger, primary_key=True, unique=True, autoincrement=True)
-    member: Union[Column, int] = Column(BigInteger)
-    author: Union[Column, int] = Column(BigInteger)
-    message: Union[Column, str] = Column(Text(collation="utf8mb4_bin"))
+    id: Union[Column, int] = Column(BigInteger, primary_key=True, unique=True, autoincrement=True)
+    member_id: Union[Column, int] = Column(BigInteger)
+    author_id: Union[Column, int] = Column(BigInteger)
+    content: Union[Column, str] = Column(Text(collation="utf8mb4_bin"))
     timestamp: Union[Column, datetime] = Column(DateTime)
 
     @staticmethod
-    async def create(member: int, message: str, author: int):
+    async def create(member_id: int, author_id: int, content: str) -> UserNote:
         row = UserNote(
-            member=member,
-            message=message,
-            author=author,
+            member_id=member_id,
+            author_id=author_id,
+            content=content,
             timestamp=datetime.utcnow(),
         )
         await db.add(row)
