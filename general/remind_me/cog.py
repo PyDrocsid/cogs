554635f409a0ae0d1099ce577f5be336d9e9d12e
Tristan <45330667+Tristan-H11@users.noreply.github.com>
2021-06-14 22:46:25 +0200

    Fixed code style

--- general/remind_me/cog.py
+++ general/remind_me/cog.py
@@ -1,18 +1,21 @@
+from discord import Message, Member, PartialEmoji
+
 from PyDrocsid.cog import Cog
 from PyDrocsid.emojis import name_to_emoji
 from PyDrocsid.translations import t
-from discord import Message, Member, PartialEmoji
 
 tg = t.g
 t = t.remindme
 
-EMOJI = {name_to_emoji["star"],
-         name_to_emoji["mailbox"],
-         name_to_emoji["e_mail"],
-         name_to_emoji["envelope"],
-         name_to_emoji["incoming_envelope"],
-         name_to_emoji["envelope_with_arrow"],
-         name_to_emoji["floppy_disk"]}
+EMOJI = {
+    name_to_emoji["star"],
+    name_to_emoji["mailbox"],
+    name_to_emoji["e_mail"],
+    name_to_emoji["envelope"],
+    name_to_emoji["incoming_envelope"],
+    name_to_emoji["envelope_with_arrow"],
+    name_to_emoji["floppy_disk"],
+}
 
 
 class RemindMeCog(Cog, name="RemindMe"):
