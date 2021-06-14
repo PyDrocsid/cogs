265648bdc95746dd5344404a3058f484840ca505
Tristan <triddel02@gmx.de>
2021-06-14 22:34:44 +0200

    Added a remind me functionality via message reaction.

---
+++ general/remind_me/cog.py
@@ -0,0 +1,25 @@
+from PyDrocsid.cog import Cog
+from PyDrocsid.emojis import name_to_emoji
+from PyDrocsid.translations import t
+from discord import Message, Member, PartialEmoji
+
+tg = t.g
+t = t.reactionpin
+
+EMOJI = {name_to_emoji["star"],
+         name_to_emoji["mailbox"],
+         name_to_emoji["e_mail"],
+         name_to_emoji["envelope"],
+         name_to_emoji["incoming_envelope"],
+         name_to_emoji["envelope_with_arrow"],
+         name_to_emoji["floppy_disk"]}
+
+
+class ReactionPinCog(Cog, name="RemindMe"):
+    CONTRIBUTORS = []
+
+    async def on_raw_reaction_add(self, message: Message, emoji: PartialEmoji, member: Member):
+        if str(emoji) not in EMOJI or member.bot or message.guild is None:
+            return
+
+        await Member.send(message.content, embed=message.embeds[0] if message.embeds else None)
