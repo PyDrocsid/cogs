25cff4d15545b23ed4dc2005ed646b2f1dfd1b6f
Tristan <45330667+Tristan-H11@users.noreply.github.com>
2021-06-14 22:48:40 +0200

    Python für Anfänger << Türkisch für Anfänger

--- general/remind_me/cog.py
+++ general/remind_me/cog.py
@@ -7,15 +7,15 @@ from PyDrocsid.translations import t
 tg = t.g
 t = t.remindme
 
-+EMOJI = {
-+    name_to_emoji["star"],
-+    name_to_emoji["mailbox"],
-+    name_to_emoji["e_mail"],
-+    name_to_emoji["envelope"],
-+    name_to_emoji["incoming_envelope"],
-+    name_to_emoji["envelope_with_arrow"],
-+    name_to_emoji["floppy_disk"],
-+}
+EMOJI = {
+    name_to_emoji["star"],
+    name_to_emoji["mailbox"],
+    name_to_emoji["e_mail"],
+    name_to_emoji["envelope"],
+    name_to_emoji["incoming_envelope"],
+    name_to_emoji["envelope_with_arrow"],
+    name_to_emoji["floppy_disk"],
+}
 
 
 class RemindMeCog(Cog, name="RemindMe"):
