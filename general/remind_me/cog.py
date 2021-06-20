f8901b09143f6224f7b61639324f6cf323882679
Tristan <triddel02@gmx.de>
2021-06-20 21:34:09 +0200

    Removed the remindMe-functionality because of the possibility to steal temporary pictures.

--- general/remind_me/cog.py
+++ general/remind_me/cog.py
@@ -43,18 +43,6 @@ class RemindMeCog(Cog, name="RemindMe"):
         if str(emoji) not in EMOJIS or member.bot or message.guild is None:
             return
 
-        if message.attachments:
-            try:
-                message_to_user = await member.send("\n".join(attachment.url for attachment in message.attachments))
-                await message_to_user.add_reaction(name_to_emoji["wastebasket"])
-            except Forbidden:
-                try:
-                    await message.remove_reaction(emoji, member)
-                    return
-                except Forbidden:
-                    await send_alert(message.guild, t.cannot_send(message.jump_url))
-                    return
-
         embed = message.embeds[0] if message.embeds else None
 
         if message.content or embed:
