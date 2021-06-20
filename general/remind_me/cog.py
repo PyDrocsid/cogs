71ce8bff100bc2c251ecc3daf3cebc805bfc2c9b
Tristan <triddel02@gmx.de>
2021-06-20 21:30:58 +0200

    Fixes with tnt and defelo.

--- general/remind_me/cog.py
+++ general/remind_me/cog.py
@@ -35,13 +35,18 @@ class RemindMeCog(Cog, name="RemindMe"):
         Removes the user reaction, if the user does not accept private messages.
         Sends an alert in case of missing permissions.
         """
+
+        if message.guild is None and str(emoji) == name_to_emoji["wastebasket"] and message.author == self.bot.user:
+            await message.delete()
+            raise StopEventHandling
+
         if str(emoji) not in EMOJIS or member.bot or message.guild is None:
             return
 
         if message.attachments:
             try:
                 message_to_user = await member.send("\n".join(attachment.url for attachment in message.attachments))
-                message_to_user.add_reaction(name_to_emoji["wastebasket"])
+                await message_to_user.add_reaction(name_to_emoji["wastebasket"])
             except Forbidden:
                 try:
                     await message.remove_reaction(emoji, member)
@@ -55,13 +60,9 @@ class RemindMeCog(Cog, name="RemindMe"):
         if message.content or embed:
             try:
                 message_to_user = await member.send(message.content, embed=embed)
-                message_to_user.add_reaction(name_to_emoji["wastebasket"])
+                await message_to_user.add_reaction(name_to_emoji["wastebasket"])
             except Forbidden:
                 try:
                     await message.remove_reaction(emoji, member)
                 except Forbidden:
                     await send_alert(message.guild, t.cannot_send(message.jump_url))
-
-        if await check_wastebasket(message_to_user, member, emoji, t.created_by):
-            await message.delete()
-            raise StopEventHandling
