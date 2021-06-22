bbf6ffd3369bbb9ebda842dcb5958d5657a128a3
Florian Sabonchi <sabonchi@posteo.de>
2021-06-22 20:29:12 +0200

    first draft for auto_delete_messages

---
+++ moderation/auto_delete_messages/cog.py
@@ -0,0 +1,20 @@
+from PyDrocsid.cog import Cog
+from discord import TextChannel
+from discord.ext import commands
+from discord.ext.commands import Context, UserInputError, guild_only
+
+from ...contributor import Contributor
+
+
+class AutoDeleteMessages(Cog, name="Auto Delete Messages"):
+    CONTRIBUTORS = [Contributor.Florian, Contributor.Defelo]
+
+    @guild_only()
+    @commands.group(aliases=["adm"])
+    async def auto_delete_messages(self, ctx: Context):
+        if ctx.invoked_subcommand is None:
+            raise UserInputError
+
+    @auto_delete_messages.command(aliases=["add"])
+    async def add_channel(self, ctx: Context, channel: TextChannel):
+        pass
