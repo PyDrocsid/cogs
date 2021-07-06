e06405818726ebde2605ca4ff296f20b5cbfd160
Tristan <45330667+Tristan-H11@users.noreply.github.com>
2021-07-06 20:20:52 +0200

    Apply comment suggestions from code review
    Co-authored-by: Felix <41747605+Defelo@users.noreply.github.com>

--- general/remind_me/cog.py
+++ general/remind_me/cog.py
@@ -23,17 +23,15 @@ EMOJIS = {
 
 
 class RemindMeCog(Cog, name="RemindMe"):
-    """
-    Adds a "Remind Me"-functionality by sending the user a message they reacted on.
-    """
+    """Add a "Remind Me"-functionality by sending the user a message they reacted on."""
 
     CONTRIBUTORS = [Contributor.Tristan]
 
     async def on_raw_reaction_add(self, message: Message, emoji: PartialEmoji, member: Member):
         """
-        Checks, if the reaction is in a list of set emojis. If so, the message is sent via private message.
-        Removes the user reaction, if the user does not accept private messages.
-        Sends an alert in case of missing permissions.
+        Check, if the reaction is in a list of set emojis. If so, the message is sent as a direct message to the user.
+        Remove the user reaction, if the user does not accept direct messages.
+        Send an alert in case of missing permissions.
         """
         if str(emoji) not in EMOJIS or member.bot or message.guild is None:
             return
