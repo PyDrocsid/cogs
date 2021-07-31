5b79411741c242f0dfa092b0035fdd27e82f750f
Florian Sabonchi <sabonchi@posteo.de>
2021-07-31 12:42:58 +0200

    updated model

--- general/auto_reaction/models.py
+++ general/auto_reaction/models.py
@@ -3,8 +3,14 @@ from __future__ import annotations
 from typing import Union
 
 from PyDrocsid.database import db
-from sqlalchemy import Column, Text, BigInteger, Table, ForeignKey, Integer
+from sqlalchemy import Column, Text, BigInteger, Table, ForeignKey
 from sqlalchemy.orm import relationship
+
+
+# auto_reaction_link = Table('auto_reaction_link', db.Base.metadata,
+#                            Column('channel_id', ForeignKey('auto_reaction_channel.id'), primary_key=True),
+#                            Column('autoreaction_id', ForeignKey('auto_reaction.id'), primary_key=True)
+#                            )
 
 
 class AutoReactionChannel(db.Base):
@@ -17,7 +23,7 @@ class AutoReactionChannel(db.Base):
     @staticmethod
     async def create(channel_id: int) -> AutoReactionChannel:
         row = AutoReactionChannel(
-            channel_id=channel_id,
+            channel=channel_id,
         )
         await db.add(row)
         return row
@@ -30,17 +36,29 @@ class AutoReaction(db.Base):
     reaction: Union[Column, str] = Column(Text(collation="utf8mb4_bin"))
     channels = relationship('AutoReactionChannel', secondary='auto_reaction_link', back_populates='reactions')
 
+    @staticmethod
+    async def create(reaction: str) -> AutoReaction:
+        row = AutoReaction(
+            reaction=reaction,
+        )
+        await db.add(row)
+        return row
 
-class Link(db.Base):
+
+class AutoReactionLink(db.Base):
     __tablename__ = 'auto_reaction_link'
+    id: Union[Column, int] = Column(BigInteger, primary_key=True, unique=True, autoincrement=True)
 
     channel_id = Column(
         BigInteger,
-        ForeignKey('auto_reaction_channel.id'),
-        primary_key=True)
+        ForeignKey('auto_reaction_channel.id'))
 
     autoreaction_id = Column(
         BigInteger,
-        ForeignKey('auto_reaction.id'),
-        primary_key=True)
+        ForeignKey('auto_reaction.id'))
 
+    @staticmethod
+    async def create(channel_id: int, autoreaction_id: int) -> AutoReactionLink:
+        link = AutoReactionLink(channel_id=channel_id, autoreaction_id=autoreaction_id)
+        await db.add(link)
+        return link
