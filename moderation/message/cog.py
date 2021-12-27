4e4c05de662d812bc4b4ec0f6abe1e67c2eff6eb
NekoFanatic <83883849+NekoFanatic@users.noreply.github.com>
2021-12-27 21:14:52 +0100

    Don't change embed color if not specified #134

--- moderation/message/cog.py
+++ moderation/message/cog.py
@@ -178,6 +178,8 @@ class MessageCog(Cog, name="Message Commands"):
 
         if color is not None:
             send_embed.colour = color
+        elif color is None and message.embeds[0].color is not Embed.Empty:
+            send_embed.colour = int(("0x" + str(message.embeds[0].color).split("#")[1]), 16)
 
         await message.edit(content=None, files=[], embed=send_embed)
         embed.description = t.msg_edited
