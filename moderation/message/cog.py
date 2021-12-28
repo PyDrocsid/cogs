ff89e8bcbc9714083414eea57edaec2260052d96
NekoFanatic <83883849+NekoFanatic@users.noreply.github.com>
2021-12-28 10:16:13 +0100

    Fixed error if no message with embed was selected

--- moderation/message/cog.py
+++ moderation/message/cog.py
@@ -178,8 +178,9 @@ class MessageCog(Cog, name="Message Commands"):
 
         if color is not None:
             send_embed.colour = color
-        elif color is None and message.embeds[0].color is not Embed.Empty:
-            send_embed.colour = message.embeds[0].color
+        elif color is None and message.embeds:
+            if message.embeds[0].color is not Embed.Empty:
+                send_embed.colour = message.embeds[0].color
 
         await message.edit(content=None, files=[], embed=send_embed)
         embed.description = t.msg_edited
