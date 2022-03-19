1967450a1ed085f44d9e16d87a019e30408757ba
NekoFanatic <83883849+NekoFanatic@users.noreply.github.com>
2022-03-19 22:07:12 +0100

    now makes only one request if an endpoint is used

--- community/nekosbest/cog.py
+++ community/nekosbest/cog.py
@@ -1,6 +1,6 @@
 from aiohttp import ClientSession
 
-from discord import Embed
+from discord import Embed, Member
 from discord.ext import commands
 from discord.ext.commands import Context
 
@@ -20,15 +20,17 @@ async def get_endpoints() -> list:
         resp = await session.get("https://nekos.best/api/v2/endpoints")
         endpoints: dict = await resp.json()
 
-        return [key for key in endpoints.keys()]
+        return list(endpoints.keys())
 
 
-async def get_request(endpoint: str) -> dict:
+async def get_request(endpoint: str) -> dict | None:
     async with ClientSession() as session:
         resp = await session.get(f"https://nekos.best/api/v2/{endpoint}")
         result: dict = await resp.json()
-
-        return result["results"][0]
+        try:
+            return result["results"][0]
+        except KeyError:
+            return None
 
 
 class NekosBestCog(Cog, name="NekosBest"):
@@ -37,24 +39,23 @@ class NekosBestCog(Cog, name="NekosBest"):
     @commands.command(name="nekosbest", aliases=["nb"])
     @docs(t.commands.nekosbest)
     async def nekosbest(self, ctx: Context, endpoint: str | None = None):
-        endpoints = await get_endpoints()
+        result: dict = await get_request(endpoint)
 
-        if not endpoint or endpoint not in endpoints:
+        if result:
+            if "anime_name" in result:
+                embed = Embed(title=endpoint, description=result["anime_name"], color=Colors.NekosBest)
+            else:
+                embed = Embed(
+                    title=endpoint,
+                    description=t.description(result["source_url"], result["artist_name"], result["artist_href"]),
+                    color=Colors.NekosBest,
+                )
+            embed.set_image(url=result["url"])
+
+        else:
+            endpoints = await get_endpoints()
             embed = Embed(
                 title=t.endpoints, description=t.endpoint_list("`, `".join(endpoints)), color=Colors.NekosBest
             )
-            await send_long_embed(ctx.channel, embed)
-            return
-
-        result: dict = await get_request(endpoint)
-        if "anime_name" in result:
-            embed = Embed(title=endpoint, description=result["anime_name"], color=Colors.NekosBest)
-        else:
-            embed = Embed(
-                title=endpoint,
-                description=t.description(result["source_url"], result["artist_name"], result["artist_href"]),
-                color=Colors.NekosBest,
-            )
-        embed.set_image(url=result["url"])
 
         await send_long_embed(ctx.channel, embed)
