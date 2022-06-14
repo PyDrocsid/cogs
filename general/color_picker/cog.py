e8bf57fcd280f9707ad3dd4d0b1b36563106e787
NekoFanatic <83883849+NekoFanatic@users.noreply.github.com>
2022-06-14 22:24:07 +0200

    Wrote documentation + changed some code style

--- general/color_picker/cog.py
+++ general/color_picker/cog.py
@@ -55,6 +55,7 @@ class ColorPickerCog(Cog, name="Color Picker"):
     RE_HEX = re.compile(r"^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$")
     RE_RGB = re.compile(r"^rgb\(([0-9]{1,3}), *([0-9]{1,3}), *([0-9]{1,3})\)$")
     RE_HSV = re.compile(r"^hsv\(([0-9]{1,3}), *([0-9]{1,3}), *([0-9]{1,3})\)$")
+    RE_HSL = re.compile(r"^hsl\(([0-9]{1,3}), *([0-9]{1,3}), *([0-9]{1,3})\)$")
 
     @commands.command(name="color_picker", aliases=["cp", "color"])
     @docs(t.commands.color_picker)
@@ -62,27 +63,37 @@ class ColorPickerCog(Cog, name="Color Picker"):
 
         if color_re := self.RE_HEX.match(color):
             rgb = _hex_to_color(color_re.group(1))
+            rgb = _to_floats([(rgb[0], 255), (rgb[1], 255), (rgb[2], 255)])
 
         elif color_re := self.RE_RGB.match(color):
             rgb = _to_rgb((color_re.group(1), color_re.group(2), color_re.group(3)))
+            rgb = _to_floats([(rgb[0], 255), (rgb[1], 255), (rgb[2], 255)])
 
         elif color_re := self.RE_HSV.match(color):
-            values = _to_floats([(color_re.group(1), 359), (color_re.group(2), 100), (color_re.group(3), 100)])
+            values = _to_floats([(color_re.group(1), 360), (color_re.group(2), 100), (color_re.group(3), 100)])
             rgb = colorsys.hsv_to_rgb(values[0], values[1], values[2])
-            rgb = tuple(int(color * 255) for color in rgb)
+
+        elif color_re := self.RE_HSL.match(color):
+            values = _to_floats([(color_re.group(1), 360), (color_re.group(2), 100), (color_re.group(3), 100)])
+            rgb = colorsys.hls_to_rgb(values[0], values[2], values[1])
 
         else:
             raise CommandError(t.error_parse_color_example(color))
 
-        color_hex = f"{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
-        hsv = colorsys.rgb_to_hsv(*rgb)
-        h, s, v = hsv
-        hsv = (int(h * 360), int(s * 100), int(v))
+        h, s, v = colorsys.rgb_to_hsv(*rgb)
+        hsv = (int(round(h * 360, 0)), int(round(s * 100, 0)), int(round(v * 100, 0)))
 
-        embed: Embed = Embed(title="Color Picker", color=Colour(int(color_hex, 16)))
+        h, l, s = colorsys.rgb_to_hls(*rgb)
+        hsl = (int(round(h * 360)), int(round(s * 100)), int(round(l * 100)))
+
+        rgb = tuple(int(color * 255) for color in rgb)
+        color_hex = f"{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
+
+        embed: Embed = Embed(title=t.embed.title, color=Colour(int(color_hex, 16)))
         embed.set_image(url=f"https://singlecolorimage.com/get/{color_hex}/300x50")
         embed.add_field(name="HEX", value=f"`#{color_hex}`")
         embed.add_field(name="RGB", value=f"`rgb{rgb}`")
         embed.add_field(name="HSV", value=f"`hsv{hsv}`")
+        embed.add_field(name="HSL", value=f"`hsl{hsl}`")
 
         await reply(ctx, embed=embed)
--- general/color_picker/documentation.md
+++ general/color_picker/documentation.md
@@ -1,4 +1,16 @@
 # Color Picker
 
-<!-- markdownlint-disable-next-line MD036 -->
-*Work in Progress*
+Contains a command to transform different color forms into each other
+
+
+## `color_picker`
+
+```css
+.[color_picker|cp|color] <color>
+```
+
+Arguments:
+
+| Argument | Required | Description                                                                                                                           |
+|:--------:|:--------:|:--------------------------------------------------------------------------------------------------------------------------------------|
+| `color`  |          | can be a hex-code or it can start with [`rgb`, `hsl` or `hsv`] and has to get in brackets behind that 3 numbers, separated by a comma |
