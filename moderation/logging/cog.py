f9657ddbdd764a32b5c3c1b29a1d6e809b581780
TheCataliasTNT2k <44349750+TheCataliasTNT2k@users.noreply.github.com>
2023-02-02 20:35:52 +0100

    linter and add user ID to message

--- moderation/logging/cog.py
+++ moderation/logging/cog.py
@@ -310,10 +310,10 @@ class LoggingCog(Cog, name="Logging"):
         if (log_channel := await self.get_logging_channel(LoggingSettings.member_leave_channel)) is None:
             return
 
-        if (join := member.joined_at):
-            await log_channel.send(t.member_left_server_with_joined(member, join.strftime("%m/%d/%Y, %H:%M:%S")))
+        if join := member.joined_at:
+            await log_channel.send(t.member_left_server_with_joined(member, member.id, join.strftime("%m/%d/%Y, %H:%M:%S")))
         else:
-            await log_channel.send(t.member_left_server(member))
+            await log_channel.send(t.member_left_server(member, member.id))
 
     @commands.group(aliases=["log"])
     @LoggingPermission.read.check
